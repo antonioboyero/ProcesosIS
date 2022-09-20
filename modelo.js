4d9ca619361bdffd89207cd4f98f0640afcc6555
ANTONIO JESÚS BOYERO PARRA <AntonioJesus.Boyero@alu.uclm.es>
2022-09-20 09:39:02 +0200

    segundo

--- modelo.js
+++ modelo.js
@@ -1,13 +1,33 @@
 function Juego(){
-	this.partidas=[];
-	this.agregarPartida=function(nombre){
-		this.partidas.push(new Partida(nombre))
+	this.partidas={};	//array asociativo
+	this.usuarios={};	//array asociativo
+
+	this.agregarUsuario=function(nick){
+		if(!this.usuarios[nick]){									//miramos si existe el usuario y si no lo creo
+			this.usuarios[nick]=new this.usuarios(nick,this)
+		}
 	}
-	this.eliminarPartida=funciton(nombre){
-		//todo
+
+	this.eliminarUsuario=function(nick){
+		delete this.usuarios[nick];
+	}
+
+	this.crearPartida=function(nick){
+		//obtener codigo único
+		//crear la partida con propietario nick
+		//devolver el codigo o la partida
+		
 	}
 }
 
-function Partida(nombre){
-	this.nombre=nombre;
+function Partida(){
+	this.codigo;
+}
+
+function Usuario(nick, juego){
+	this.nick=nick;
+	this.juego=juego; //esto es lo que hemos dicho que es navegable, por eso hemos añadido end1 juego 1 etc
+	this.crearPartida=function(){
+		this.juego.crearPartida(this.nick)
+	}
 }
